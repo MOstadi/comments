2edde372b1ca6896dd428fe9787d6bbce235a86f
MOstadi <7m.ostadi5@gmail.com>
2021-06-10 02:38:24 +0430

    Add files via upload
    add fa lang.

---
+++ resources/lang/fa/comments.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'there_are_no_comments' => 'هیچ نظری ثبت نشده است.',
+    'authentication_required' => 'نیازمند ورود به حساب کاربری',
+    'you_must_login_to_post_a_comment' => 'برای ارسال نظر باید وارد حساب کاربری خود شوید.',
+    'log_in' => 'ورود به حساب کاربری',
+    'reply' => 'پاسخ',
+    'edit' => 'ویرایش',
+    'delete' => 'حذف',
+    'edit_comment' => 'ویرایش',
+    'update_your_message_here' => 'پیام خود را ویرایش کنید:',
+    'update' => 'ثبت',
+    'cancel' => 'انصراف',
+    'reply_to_comment' => 'پاسخ دادن',
+    'enter_your_message_here' => 'پیام خود را در این جا بنویسید:',
+    'markdown_cheatsheet' => '<a target="_blank" href=":url">نگارشی</a> راهنمای.',
+    'submit' => 'ارسال',
+    'your_message_is_required' => 'متن پیام نباید خالی بماند.',
+    'enter_your_email_here' => 'ایمیل خود را وارد کنید:',
+    'enter_your_name_here' => 'نام خود را وارد کنید:',
+];
